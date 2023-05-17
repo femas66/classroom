d8eaa41796066ec2d2e1ceefc2781e4a026cf59a
femas <111944157+femas66@users.noreply.github.com>
2023-05-17 10:24:11 +0700

    memperbaiki tampilan login register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,30 +1,68 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Register</title>
-</head>
-<body>
-  <h2>Register</h2>
-  <hr>
-  <form action="{{ route('register.post') }}" method="post">
-    @csrf
-    @method('POST')
-    <input type="text" name="name" id="name" placeholder="Name" value="{{ old('name') }}">
-    @error('name')
-      {{ $message }}
-    @enderror
-    <input type="email" name="email" id="email" placeholder="Email" value="{{ old('email') }}">
-    @error('email')
-      {{ $message }}
-    @enderror
-    <input type="password" name="password" id="password" placeholder="*****">
-    @error('password')
-      {{ $message }}
-    @enderror
-    <button type="submit">Register</button>
-  </form>
-</body>
+  <head>
+  	<title>Register</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	
+	<link rel="stylesheet" href="/css/login.css">
+
+	</head>
+	<body>
+	<section class="ftco-section">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-md-6 text-center mb-5">
+					<h2 class="heading-section">Register</h2>
+				</div>
+			</div>
+			<div class="row justify-content-center">
+				<div class="col-md-12 col-lg-10">
+					<div class="wrap d-md-flex">
+						<div class="img" style="background-image: url(/img/bg-1.jpg);">
+			      </div>
+						<div class="login-wrap p-4 p-md-5">
+			      	<div class="d-flex">
+			      		<div class="w-100">
+			      			<h3 class="mb-4">Sign Up</h3>
+			      		</div>
+								<div class="w-100">
+									<p class="social-media d-flex justify-content-end">
+										<a href="#" class="social-icon d-flex align-items-center justify-content-center"><span class="fa fa-facebook"></span></a>
+										<a href="#" class="social-icon d-flex align-items-center justify-content-center"><span class="fa fa-twitter"></span></a>
+									</p>
+								</div>
+			      	</div>
+							<form action="{{ route('register.post') }}" class="signin-form" method="POST">
+                @csrf
+                <div class="form-group mb-3">
+			      			<label class="label" for="name">Name</label>
+			      			<input type="text" class="form-control" placeholder="Nama" required name="name">
+			      		</div>
+			      		<div class="form-group mb-3">
+			      			<label class="label" for="email">Email</label>
+			      			<input type="email" class="form-control" placeholder="Email" required name="email">
+			      		</div>
+		            <div class="form-group mb-3">
+		            	<label class="label" for="password">Password</label>
+		              <input type="password" class="form-control" placeholder="Password" required name="password">
+		            </div>
+		            <div class="form-group">
+		            	<button type="submit" class="form-control btn btn-primary rounded submit px-3">Sign Up</button>
+		            </div>
+		          </form>
+		          <p class="text-center">Have a account? <a data-toggle="tab" href="{{ route('login') }}">Sign In</a></p>
+		        </div>
+		      </div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	</body>
 </html>
+
